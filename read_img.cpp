5ff241f9917f946c8a0cc60514783434428e8d9e
gtm2122 <gtm2122@github.com>
2021-11-23 10:21:04 -0500

    figured out indexing

--- read_img.cpp
+++ read_img.cpp
@@ -17,30 +17,34 @@ int main(){
  cout<<arr.shape[1]<<endl;
  cout<<arr.shape[2]<<endl;
  //cout<<arr.shape;
- for (int i = 0 ; i < 10*10*10; ++i) 
-  
-   cout<<3.0*loaded_arr[i]; 
 
- 
  double z,r,c;
  unsigned seed1 = std::chrono::system_clock::now().time_since_epoch().count();
  default_random_engine drei(seed1);
  //mt19937 generator(seed1);
  
- uniform_real_distribution<float> dist(0.1,1.5);
- z = dist(drei);
- r = dist(drei);
- c = dist(drei);
- //cout<<"zrc"<<endl;
- //cout<<z<<endl<<r<<endl<<c<<endl;
+ uniform_real_distribution<float> dist(1,2);
+ z = 1;//dist(drei);
+ r = 1;//dist(drei);
+ c = 2;//dist(drei);
+ cout<<z<<endl<<r<<endl<<c<<endl;
 
  int Z_new = z*arr.shape[0];
  int R_new = r*arr.shape[1];
  int C_new = c*arr.shape[2];
- //cout<<"new dim = "<<Z_new<<","<<R_new<<","<<C_new<<endl;
- 
- // make new array
- 
+ cout<<"new dim = "<<Z_new<<","<<R_new<<","<<C_new<<endl;
+ vector<complex<double>> new_arr(Z_new*R_new*C_new);
 
+// make new array
+ 
+ for (int i = 0; i < arr.shape[0]; ++i)
+  for (int j = 0; j < arr.shape[1]; ++j)
+   for (int k = 0; k < arr.shape[2]; ++k){
+    new_arr[arr.shape[0]*int(i*z) + arr.shape[1]*int(j*r) + int(k*c)] = loaded_arr[i*arr.shape[0] + j*arr.shape[1] + k] ;
+   }
+
+ for (int i = 0 ; i< Z_new*R_new*C_new; ++i){
+  cout<<new_arr[i];
+ }
  return 0;
 }
