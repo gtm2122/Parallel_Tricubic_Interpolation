c1bd966cabd28347868ae38ccea91502544d1ad1
gtm2122 <gtm2122@github.com>
2021-11-30 19:39:49 -0500

    figured out contiguos indexing

--- read_img.cpp
+++ read_img.cpp
@@ -9,42 +9,63 @@ using std::mt19937;
 #include<complex>
 using std::vector;using std::complex;
 int main(){
- cnpy::NpyArray arr = cnpy::npy_load("a.npy");  
+ cnpy::NpyArray arr = cnpy::npy_load("four.npy");  
  
- complex<double>* loaded_arr = arr.data<std::complex<double>>();
-
- cout<<arr.shape[0]<<endl;
- cout<<arr.shape[1]<<endl;
- cout<<arr.shape[2]<<endl;
- //cout<<arr.shape;
-
- double z,r,c;
- unsigned seed1 = std::chrono::system_clock::now().time_since_epoch().count();
- default_random_engine drei(seed1);
- //mt19937 generator(seed1);
+ double *loaded_data = arr.data<double>();
  
- uniform_real_distribution<float> dist(1,2);
- z = 1;//dist(drei);
+ size_t nz = arr.shape[0];
+ size_t nr = arr.shape[1];
+ size_t nc = arr.shape[2];
+
+ std::vector<double> vec3d(nz*nr*nc);
+ 
+ for (size_t z_index = 0; z_index< nz; ++z_index){
+  for(size_t r_index = 0;r_index<nr; ++r_index){
+   for(size_t c_index=0; c_index<nc ; ++c_index){
+     vec3d[(z_index*nr + r_index)*nc + c_index] = loaded_data[(z_index*nr + r_index)*nc + c_index];
+    }
+  }
+ }
+ 
+ 
+ cnpy::npy_save("img_cpp.npy", &vec3d[0], {nz,nr,nc},"w");
+ return 0;
+ /*z = 1;//dist(drei);
  r = 1;//dist(drei);
  c = 2;//dist(drei);
- cout<<z<<endl<<r<<endl<<c<<endl;
-
- int Z_new = z*arr.shape[0];
- int R_new = r*arr.shape[1];
- int C_new = c*arr.shape[2];
- cout<<"new dim = "<<Z_new<<","<<R_new<<","<<C_new<<endl;
- vector<complex<double>> new_arr(Z_new*R_new*C_new);
-
-// make new array
  
+
+ size_t Z_new = z*nz;
+ size_t R_new = r*nr;
+ ize_t C_new = c*nc;
+ 
+ vec3d.reserve(nz);
+ for (size_t z_index = 0; z_index < nz; ++z_index){
+  vec3d.emplace_back(nr);
+  for (size_t r_index = 0; r_index < nr; ++r_index){
+   vec3d[z_index].emplace_back(nc);
+   for (size_t c_index = 0; c_index < nc; ++c_index){
+    }
+  }
+ }
+ 
+
+ cout<<"new dim = "<<Z_new<<","<<R_new<<","<<C_new<<endl;
+ std::vector<std::vector<double> new_arr(Z_new*R_new*C_new);
+ */
+// make new array
+ /*
  for (int i = 0; i < arr.shape[0]; ++i)
   for (int j = 0; j < arr.shape[1]; ++j)
    for (int k = 0; k < arr.shape[2]; ++k){
-    new_arr[arr.shape[0]*int(i*z) + arr.shape[1]*int(j*r) + int(k*c)] = loaded_arr[i*arr.shape[0] + j*arr.shape[1] + k] ;
+    new_arr[Z_new*int(i*z) + R_new*int(j*r) + int(k*c)] = loaded_arr[i*arr.shape[0] + j*arr.shape[1] + k] ;
    }
-
+*
  for (int i = 0 ; i< Z_new*R_new*C_new; ++i){
   cout<<new_arr[i];
  }
+
+ cnpy::npy_save("img_cpp.npy",&new_arr,{Z_new,R_new,C_new},"w");
+*/
  return 0;
 }
